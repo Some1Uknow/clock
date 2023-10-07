13c77032a7e7d6a245e00e4a8c792f0bb384ba0d
Some1Uknow <raghu250407@example.com>
2023-10-07 09:55:50 +0530

    removed commented code

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-// document.querySelector(".btn").addEventListener("click", () => {
-//     var s = 0;
-//     var m = 0;
-//     var h = 0;
-//     const timerElement = document.querySelector(".time");
-
-//     function updateTimer() {
-//         if (s <= 59) {
-//             timerElement.textContent = "00 Hrs : 00 Minutes : " + s + " seconds";
-//             s = s+1;
-//             setTimeout(updateTimer, 1000);
-//         }
-//     }
-
-//     function updateTimerMinute() {
-//         if (m <= 59) {
-//             timerElement.textContent = "00 Hrs : " + m + " Minutes : " + s + " seconds";
-//             m = m+1;
-//             setTimeout(updateTimerMinute, 60000);
-//         }
-//     }
-
-//     function updateTimerHour() {
-//         if (h <= 59) {
-//             timerElement.textContent = h + " Hrs : " + m + " Minutes : " + s + " seconds";
-//             h = h+1;
-//             setTimeout(updateTimerHour, 3600000);
-//         }
-//     }
-
-//     updateTimer();
-// });
-
 document.querySelector(".btn").addEventListener("click", () => {
     var s = 0;
     var m = 0;
